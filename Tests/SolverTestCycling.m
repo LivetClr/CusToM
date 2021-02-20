16bb4513e2547aa96e5100d60bbca2345b82e1a5
LivetClr <claire.livet@ens-rennes.fr>
2021-02-20 18:05:50 +0100

    Always trying to understand test procedures

---
+++ Tests/SolverTestCycling.m
@@ -0,0 +1,41 @@
+classdef SolverTestCycling < matlab.unittest.TestCase
+    methods(Test)
+        function testglobal(testCase)
+            
+        Main;
+        
+        path_expectation = 'JOTH_Fin_125HzModif_Expected';
+        path =  'JOTH_Fin_125HzModif';
+        
+        
+        InverseKinematicsResults_Expected = load([path_expectation, '/InverseKinematicsResults.mat']);
+        InverseKinematicsResults = load([path, '/InverseKinematicsResults.mat']);
+        
+        testCase.assertEqual(InverseKinematicsResults_Expected,InverseKinematicsResults)
+        
+        
+        ExperimentalData_Expected = load([path_expectation, '/ExperimentalData.mat']);
+        ExperimentalData = load([path, '/ExperimentalData.mat']);
+        
+        testCase.assertEqual(ExperimentalData_Expected,ExperimentalData)
+        
+        ExternalForcesComputationResults_Expected = load([path_expectation, '/ExternalForcesComputationResults.mat']);
+        ExternalForcesComputationResults = load([path, '/ExternalForcesComputationResults.mat']);
+        
+        testCase.assertEqual(ExternalForcesComputationResults_Expected,ExternalForcesComputationResults)
+        
+        
+        InverseDynamicsResults_Expected = load([path_expectation, '/InverseDynamicsResults.mat']);
+        InverseDynamicsResults = load([path, '/InverseDynamicsResults.mat']);
+        
+        testCase.assertEqual(InverseDynamicsResults_Expected,InverseDynamicsResults)
+        
+        
+        MuscleForcesComputationResults_Expected = load([path_expectation, '/MuscleForcesComputationResults.mat']);
+        MuscleForcesComputationResults = load([path, '/MuscleForcesComputationResults.mat']);
+        
+        testCase.assertEqual(MuscleForcesComputationResults_Expected,MuscleForcesComputationResults)
+        end
+    end
+end
+
--- Tests/test_Cycling.m
+++ Tests/test_Cycling.m
@@ -1,38 +1,4 @@
 %% Test case from Cycling Example
 
-testCase = matlab.unittest.TestCase.forInteractiveUse;
-
-Main;
-
-path_expectation = 'JOTH_Fin_125HzModif_Expected';
-path =  'JOTH_Fin_125HzModif';
-
-
-InverseKinematicsResults_Expected = load([path_expectation, '/InverseKinematicsResults.mat']);
-InverseKinematicsResults = load([path_expectation, '/InverseKinematicsResults.mat']);
-
-assertEqual(testCase,InverseKinematicsResults_Expected,InverseKinematicsResults)
-
-
-ExperimentalData_Expected = load([path_expectation, '/ExperimentalData.mat']);
-ExperimentalData = load([path_expectation, '/ExperimentalData.mat']);
-
-assertEqual(testCase,ExperimentalData_Expected,ExperimentalData)
-
-ExternalForcesComputationResults_Expected = load([path_expectation, '/ExternalForcesComputationResults.mat']);
-ExternalForcesComputationResults = load([path_expectation, '/ExternalForcesComputationResults.mat']);
-
-assertEqual(testCase,ExternalForcesComputationResults_Expected,ExternalForcesComputationResults)
-
-
-InverseDynamicsResults_Expected = load([path_expectation, '/InverseDynamicsResults.mat']);
-InverseDynamicsResults = load([path_expectation, '/InverseDynamicsResults.mat']);
-
-assertEqual(testCase,InverseDynamicsResults_Expected,InverseDynamicsResults)
-
-
-MuscleForcesComputationResults_Expected = load([path_expectation, '/MuscleForcesComputationResults.mat']);
-MuscleForcesComputationResults = load([path_expectation, '/MuscleForcesComputationResults.mat']);
-
-assertEqual(testCase,MuscleForcesComputationResults,MuscleForcesComputationResults)
-
+testCase  = SolverTestCycling;
+res = run(testCase);
