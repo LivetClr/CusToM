248df661e26688d29f7c47f334a41ebad2b4fc24
LivetClr <claire.livet@ens-rennes.fr>
2021-02-21 12:05:41 +0100

    Enjoying tolerances

--- Tests/Cycling/SolverTestCycling.m
+++ Tests/Cycling/SolverTestCycling.m
@@ -17,29 +17,29 @@ classdef SolverTestCycling < matlab.unittest.TestCase
             InverseKinematicsResults_Expected = load([path_expectation, '/InverseKinematicsResults.mat']);
             InverseKinematicsResults = load([path, '/InverseKinematicsResults.mat']);
             
-            testCase.assertEqual(InverseKinematicsResults,InverseKinematicsResults_Expected,'RelTol',1e-4);
+            testCase.assertEqual(InverseKinematicsResults,InverseKinematicsResults_Expected,'RelTol',1e-5,'AbsTol',1e-6);
             
                
             ExperimentalData_Expected = load([path_expectation, '/ExperimentalData.mat']);
             ExperimentalData = load([path, '/ExperimentalData.mat']);
             
-            testCase.assertEqual(ExperimentalData,ExperimentalData_Expected,'RelTol',1e-4);
+            testCase.assertEqual(ExperimentalData,ExperimentalData_Expected,'RelTol',1e-5,'AbsTol',1e-6);
 
              ExternalForcesComputationResults_Expected = load([path_expectation, '/ExternalForcesComputationResults.mat']);
             ExternalForcesComputationResults = load([path, '/ExternalForcesComputationResults.mat']);
             
-            testCase.assertEqual(ExternalForcesComputationResults,ExternalForcesComputationResults_Expected,'RelTol',1e-4);
+            testCase.assertEqual(ExternalForcesComputationResults,ExternalForcesComputationResults_Expected,'RelTol',1e-5,'AbsTol',1e-6);
           
             
              InverseDynamicsResults_Expected = load([path_expectation, '/InverseDynamicsResults.mat']);
             InverseDynamicsResults = load([path, '/InverseDynamicsResults.mat']);
             
-            testCase.assertEqual(InverseDynamicsResults,InverseDynamicsResults_Expected,'RelTol',1e-4);
+            testCase.assertEqual(InverseDynamicsResults,InverseDynamicsResults_Expected,'RelTol',1e-5,'AbsTol',1e-6);
 
             MuscleForcesComputationResults_Expected = load([path_expectation, '/MuscleForcesComputationResults.mat']);
             MuscleForcesComputationResults = load([path, '/MuscleForcesComputationResults.mat']);
             
-            testCase.assertEqual(MuscleForcesComputationResults,MuscleForcesComputationResults_Expected,'RelTol',1e-4);
+            testCase.assertEqual(MuscleForcesComputationResults,MuscleForcesComputationResults_Expected,'RelTol',1e-5,'AbsTol',1e-6);
             
         end
         
@@ -65,29 +65,29 @@ classdef SolverTestCycling < matlab.unittest.TestCase
             InverseKinematicsResults_Expected = load([path_expectation, '/InverseKinematicsResults.mat']);
             InverseKinematicsResults = load([path, '/InverseKinematicsResults.mat']);
             
-            testCase.assertEqual(InverseKinematicsResults,InverseKinematicsResults_Expected,'RelTol',1e-4);
+            testCase.assertEqual(InverseKinematicsResults,InverseKinematicsResults_Expected,'RelTol',1e-5,'AbsTol',1e-6);
             
             ExperimentalData_Expected = load([path_expectation, '/ExperimentalData.mat']);
             ExperimentalData = load([path, '/ExperimentalData.mat']);
             
-            testCase.assertEqual(ExperimentalData,ExperimentalData_Expected,'RelTol',1e-4);
+            testCase.assertEqual(ExperimentalData,ExperimentalData_Expected,'RelTol',1e-5,'AbsTol',1e-6);
             
             ExternalForcesComputationResults_Expected = load([path_expectation, '/ExternalForcesComputationResults.mat']);
             ExternalForcesComputationResults = load([path, '/ExternalForcesComputationResults.mat']);
             
-            testCase.assertEqual(ExternalForcesComputationResults,ExternalForcesComputationResults_Expected,'RelTol',1e-4);
+            testCase.assertEqual(ExternalForcesComputationResults,ExternalForcesComputationResults_Expected,'RelTol',1e-5,'AbsTol',1e-6);
             
             
             InverseDynamicsResults_Expected = load([path_expectation, '/InverseDynamicsResults.mat']);
             InverseDynamicsResults = load([path, '/InverseDynamicsResults.mat']);
             
-            testCase.assertEqual(InverseDynamicsResults,InverseDynamicsResults_Expected,'RelTol',1e-4);
+            testCase.assertEqual(InverseDynamicsResults,InverseDynamicsResults_Expected,'RelTol',1e-5,'AbsTol',1e-6);
             
             
             MuscleForcesComputationResults_Expected = load([path_expectation, '/MuscleForcesComputationResults.mat']);
             MuscleForcesComputationResults = load([path, '/MuscleForcesComputationResults.mat']);
             
-            testCase.assertEqual(MuscleForcesComputationResults,MuscleForcesComputationResults_Expected,'RelTol',1e-4);
+            testCase.assertEqual(MuscleForcesComputationResults,MuscleForcesComputationResults_Expected,'RelTol',1e-5,'AbsTol',1e-6);
         end
     end
 end
